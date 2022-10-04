54ae8709503c51084327141abc54ddcb6ede5e35
ProByte <celayer02@outlook.com>
2022-10-04 13:23:49 +0300

    ....

---
+++ lib/const/eBay_const.dart
@@ -0,0 +1,350 @@
+import 'package:flutter/material.dart';
+
+///Proje içinde Sabiler Burada Barınır.
+
+class EbayConst {
+  //TEXTS
+
+  //eBay HOME
+
+  static const textFieldHintText = "Search on eBay";
+  static const registerText =
+      "Sign in so we can personalize you eBay experience";
+  static const elevatedButtonRegister = "Register";
+  static const elevatedButtonSignin = "Sign in";
+  static const elevatedButtonSelling = "Selling";
+  static const elevatedButtonDeals = "Deals";
+  static const elevatedButtonCategories = "Categories";
+  static const elevatedButtonSaved = "Saved";
+  static const homeDailyDealsTitle = "Daily Deals";
+  static const homeSeeAll = "See All";
+  static const homeDailyDealsText ="\$20/Mo Red Pocket \nPrepaid Phone\nPlan+Kit: Unlmtd Everything 8GB 5G/LTE";
+  static const homeDailyDealsTextFee = "US \$240.00";
+  static const homeDailyDealsTextFeeZeroOne = "US \$99.00";
+  static const homeExplorePoupularCategories = " Explore Popular Categories";
+  static const homeSneakers = " Sneakers";
+  static const homeKorean = " KoreanBeauty";
+  static const homeWristwatches = " Wristwatches";
+  static const homeCollectibles = " Collectibles";
+  static const homeSmartPhones = " SmartPhones";
+  static const homeFishing = " Fishing";
+  static const homeLikePage = " What do you think of the eBay home page ?";
+
+
+  //eBay DETAİLS
+
+  static const detailAppBarTitle="Item";
+  static const detailProductTitle ="\$20/Mo Red Pocket Prepaid Phone Plan+Kit: Unlmtd Everything 8GB \n 5G/LTE";
+  static const detailProduct360="360 Day eBay Exclusive Plan";
+  static const detailProductPocket="redpocketstore (24032)";
+  static const detailProductfed="97.5% positive feedback";
+  static const detailTextFee = "US \$240.00";
+  static const detailTextFeeS = "+US \$16,35 Shipping";
+  static const detailTextFeeSe = "approx ₺4.447 + ₺302,95 Shipping";
+
+  static const detailTextFeeSee = "Was Us \$365,00";
+
+  static const detailTextFeeSeer = "Save US\$125,00 (34%off)";
+
+  static const detailNate="Pzt, 17 Eki - Pzt, 31 Eki";
+  static const detailNateText="Est. delivery";
+
+  static const detailSold="49.821 sold";
+  static const detailReturn="Returns Accepted";
+
+  static const detailSimType="SIM Type";
+
+  static const detailSelect="Select";
+
+
+  static const detailQuan ="Quantity";
+  static const detailavabile ="12+ available";
+
+  static const detailSelectone ="1";
+
+
+  static const detailElevatedButtontextWact="Add to wacthlist";
+
+  static const detailElevatedButtontextadd="Add to cart";
+
+  static const detailElevatedButtontextBuy="Buy It Now";
+
+
+
+
+  //eBay Categories
+
+
+  static const categoriesTitle="Categories";
+
+  static const categoriesTitleAnt="Antiques";
+
+  static const categoriesTitleArt="Art";
+
+
+  //eBay Settings  ===>  AHMETİN CONST DOSYASINI BEN OLUŞTURDUM PROJE SON DK BİRLEŞTİRMEYE YETİŞMEDİ.
+
+
+  static const settingsTitle="Settings";
+
+  static const settingsAccount="Account";
+
+  static const settingsSign="Sign in";
+
+  static const settingsGeneral="General";
+
+
+
+  static const settingsTheme="Theme";
+
+
+  static const settingsBatery="Battery Saver";
+
+  static const settingsContry="Country of region";
+
+  static const settingsTr="Türkiye";
+
+  static const settingsTrans="Translation";
+
+
+
+  static const settingsSupport="Support";
+
+
+  static const settingsCustom="Customer Service";
+
+
+  static const settingsAvaible="Available only in ingilizce";
+
+
+  static const settingsAbout="About";
+
+  static const settingsUser="User Agreement";
+
+  static const settingsPri="Privacy";
+
+  static const settingsDonot="Do Not Sell My Personal Information & Adchoice";
+
+
+  static const settingsAdver="Advertising and Related Preferences";
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //İMAGES
+
+  static const imageAppBarLogo = "assets/images/Home/eBayLogo.png";
+  static const imageDailyDealsProduct = "assets/images/Detail/SimCardZero.jpg";
+  static const imageDailyDealsProductZeroOne =
+      "assets/images/Detail/SimCardZeroOne.jpg";
+  static const imageExploreSneakers = "assets/images/Home/Sneakers.webp";
+  static const imageExploreKorean = "assets/images/Home/KoreanBeauty.webp";
+  static const imageExploreWristwatches =
+      "assets/images/Home/Wristwatches.webp";
+  static const imageExploreCollectibles =
+      "assets/images/Home/Collectibles.webp";
+  static const imageExploreSmartphones = "assets/images/Home/SmartPhones.webp";
+  static const imageExploreFishing = "assets/images/Home/Fishing.webp";
+
+
+  
+  //İMAGES DETAİL
+  static const imageDetailSimCardZero="assets/images/Detail/SimCardZero.jpg";
+  static const imageDetailSimCardTwo="assets/images/Detail/SimCardTwo.jpg";
+  static const imageDetailSimCardTree="assets/images/Detail/SimCardTree.jpg";
+  static const imageDetailSimCardFor="assets/images/Detail/SimCardFor.jpg";
+  static const imageDetailSimCardFive="assets/images/Detail/SimCardFive.jpg";
+  static const imageDetailSimCardSix="assets/images/Detail/SimCardSix.jpg";
+  static const imageDetailSimCardSeven="assets/images/Detail/SimCardSeven.jpg";
+  static const imageDetailSimCardEight="assets/images/Detail/SimCardEight.jpg";
+
+
+  //İMAGES CATEGORİES
+
+
+  static const imageCategoriesZero="assets/images/Categories/Guitar.jpg";
+  static const imageCategoriesOne="assets/images/Categories/Rim.jpg";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //COLORS
+
+  static const Color colorTransparent = Colors.transparent;
+  static const Color colorWhite = Colors.white;
+  static const Color colorBlack = Colors.black;
+  static const Color colorRed = Colors.red;
+  static const Color colorYellow = Colors.yellow;
+  static const Color colorOrange = Colors.orange;
+  static const Color colorPink = Colors.pink;
+  static const Color colorGreen = Colors.green;
+  static const Color colorBlue = Colors.blue;
+  static const Color colorPurple = Colors.purple;
+  static const Color colorGrey = Colors.grey;
+  static const Color colorBrown = Colors.brown;
+  static const Color colorCyan = Colors.cyan;
+  static const Color colorIndigo = Colors.indigo;
+
+  //SİZEBOX
+
+  //HEİGHT
+
+  static const SizedBox sizedBoxH5 = SizedBox(height: 5);
+  static const SizedBox sizedBoxH10 = SizedBox(height: 10);
+  static const SizedBox sizedBoxH15 = SizedBox(height: 15);
+  static const SizedBox sizedBoxH20 = SizedBox(height: 20);
+  static const SizedBox sizedBoxH25 = SizedBox(height: 25);
+  static const SizedBox sizedBoxH30 = SizedBox(height: 30);
+  static const SizedBox sizedBoxH35 = SizedBox(height: 35);
+  static const SizedBox sizedBoxH40 = SizedBox(height: 40);
+  static const SizedBox sizedBoxH45 = SizedBox(height: 45);
+  static const SizedBox sizedBoxH50 = SizedBox(height: 50);
+  static const SizedBox sizedBoxH100 = SizedBox(height: 100);
+
+  //WİDTH
+
+  static const SizedBox sizedBoxW5 = SizedBox(width: 5);
+  static const SizedBox sizedBoxW10 = SizedBox(width: 10);
+  static const SizedBox sizedBoxW15 = SizedBox(width: 15);
+  static const SizedBox sizedBoxW20 = SizedBox(width: 20);
+  static const SizedBox sizedBoxW25 = SizedBox(width: 25);
+  static const SizedBox sizedBoxW30 = SizedBox(width: 30);
+  static const SizedBox sizedBoxW35 = SizedBox(width: 35);
+  static const SizedBox sizedBoxW40 = SizedBox(width: 40);
+  static const SizedBox sizedBoxW45 = SizedBox(width: 45);
+  static const SizedBox sizedBoxW50 = SizedBox(width: 50);
+  static const SizedBox sizedBoxW100 = SizedBox(width: 100);
+
+  //PADDİNG
+
+  static const EdgeInsets paddingAll5 = EdgeInsets.all(5);
+  static const EdgeInsets paddingAll10 = EdgeInsets.all(10);
+  static const EdgeInsets paddingAll15 = EdgeInsets.all(15);
+  static const EdgeInsets paddingAll20 = EdgeInsets.all(20);
+  static const EdgeInsets paddingAll25 = EdgeInsets.all(25);
+  static const EdgeInsets paddingAll30 = EdgeInsets.all(30);
+  static const EdgeInsets paddingAll35 = EdgeInsets.all(35);
+  static const EdgeInsets paddingAll40 = EdgeInsets.all(40);
+  static const EdgeInsets paddingAll45 = EdgeInsets.all(45);
+  static const EdgeInsets paddingAll50 = EdgeInsets.all(50);
+
+  //BORDER RADİUS
+  
+  static BorderRadius borderRadius5 = BorderRadius.circular(5);
+  static BorderRadius borderRadius10 = BorderRadius.circular(10);
+  static BorderRadius borderRadius20 = BorderRadius.circular(20);
+  static BorderRadius borderRadius30 = BorderRadius.circular(30);
+  static BorderRadius borderRadius40 = BorderRadius.circular(40);
+  static BorderRadius borderRadius50 = BorderRadius.circular(50);
+  static BorderRadius borderRadius60 = BorderRadius.circular(60);
+  static BorderRadius borderRadius70 = BorderRadius.circular(70);
+  static BorderRadius borderRadius80 = BorderRadius.circular(80);
+  static BorderRadius borderRadius90 = BorderRadius.circular(90);
+  static BorderRadius borderRadius100 = BorderRadius.circular(100);
+
+  //RADİUS
+
+  static const Radius radius5 = Radius.circular(5);
+  static const Radius radius10 = Radius.circular(10);
+  static const Radius radius15 = Radius.circular(15);
+  static const Radius radius20 = Radius.circular(20);
+  static const Radius radius25 = Radius.circular(25);
+  static const Radius radius30 = Radius.circular(30);
+  static const Radius radius35 = Radius.circular(35);
+  static const Radius radius40 = Radius.circular(40);
+  static const Radius radius45 = Radius.circular(45);
+  static const Radius radius50 = Radius.circular(50);
+  static const Radius radius55 = Radius.circular(55);
+  static const Radius radius60 = Radius.circular(60);
+  static const Radius radius65 = Radius.circular(65);
+  static const Radius radius70 = Radius.circular(70);
+  static const Radius radius75 = Radius.circular(75);
+
+
+
+
+  //DRUATİON UTİLİTY
+
+
+
+  static const durationLow= const Duration(milliseconds: 50);
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
